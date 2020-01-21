24bab7e08b2faa30dafa2d17541c20233ff9004e
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-01-21 12:24:14 -0500

    updated index

--- index.php
+++ index.php
@@ -3,42 +3,41 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-26808506-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-26808506-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-26808506-1');
+    </script>
 
-  gtag('config', 'UA-26808506-1');
-</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Culinary Healing</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/lamana.css">
+  </head>
+  <body>
 
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <title>Culinary Healing</title>
-      <meta name="description" content="">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-      <link rel="stylesheet" href="assets/css/lamana.css">
-
-    </head>
-    <body>
-      <?php include_once("nav.html"); ?>
-     
-
-      <!-- Hero -->
-        <div class="hero-image">
-          <div class="hero-text">
-            <h1>21-DAY CLEANSE</h1>
-            <h3>WANT TO EAT BETTER <br />
-            and FEEL BETTER?</h3>
-            <p>Join our 21-Day cleanse and let me show you how!</p>
-            <p>April 4 - 24, 2020</p>
-              <button type="button" class="btn btn-info">Learn More</button>
-            </div>
+    <!-- navigation -->
+    <?php include_once("nav.html"); ?>
+    
+    <!-- Hero -->
+      <div class="hero-image">
+        <div class="hero-text">
+          <h1>21-DAY CLEANSE</h1>
+          <h3>WANT TO EAT BETTER <br />
+          and FEEL BETTER?</h3>
+          <p>Join our 21-Day cleanse and let me show you how!</p>
+          <p>April 4 - 24, 2020</p>
+            <button type="button" class="btn btn-info">Learn More</button>
           </div>
+        </div>
 
       <!-- Subscribe -->
       <div class="subscribe flex-container">
@@ -59,10 +58,7 @@
         <div class="clear space"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
       </div>
   
-    
-
-
-      <!-- Cards
+      <!-- Cards -->
       <div class="container">
         <div class="card-group ">
           <div class="card shadow p-3 mb-5 bg-white rounded">
@@ -87,46 +83,27 @@
             </div>
           </div>
         </div>
-      </div> -->
+      </div> 
 
- <!-- about me -->
-      <div class="container">
-        <div class="row">
-          <div class="col-8">
-            <div class="jumbotron jumbotron-fluid">
-              <div class="container">
-                <h1 class="display-4">About LaMana</h1>
-                <p class="card-body">
-                  I grew up in the south, Durham, NC... where food was food, there was no separation of healthy food and unhealthy food, good and bad. If you could eat it and it stopped you from being hungry, it was all good. On any given day we had up to 15 people in our house to feed, and money was not abundant, so off to the local Piggly Wiggly grocery store (I am not making this name up), we went to get food to feed the family. I don't have to tell you what types of items we ended up with. But not to worry, my grandma would "put her foot" in whatever came through the door, and each meal was prepared with love and tasted delicious!! Pig feet, pork chops, fried chicken, white rice, liver and onions, chitterlings w/ hot sauce, fried fish on Fridays! Boy were we happy. Little did we know, we were setting our bodies up to fail us. How could we know? No one ever told my grandma that pork was bad, and fried foods would eventually kill us.
-                    Fast forward many years later, I studied under Queen Afua -  and as a result, cut out all meat, dairy, fast food and processed food. During that time, I learned a tremendous amount about foods and its effects on my body and I started looking at the world through very different eyes. I had gained a profound clarity about life and food's role in it. I felt better... Much better. rmed with a new found sense of purpose I wanted to know more and studied under Gabriel Cousins at the Tree of Life in Patagonia Arizona, from there, studied at the Natural Gourmet Institue for Health and Culinary Arts</p>  
-               </div>  
-              </div>  
-            </div>
-     
-       
-          <div class="col-4 profile-image" > 
-          </div>
-        </div>
- 
-      <!-- Instagram Feed -->
-      <!-- <div class="container feed">
+    <!-- Instagram Feed -->
+      <div class="container feed">
         <div id="pixlee_container"></div><script type="text/javascript">window.PixleeAsyncInit = function() {Pixlee.init({apiKey:'gN9tTSuhATrT4NaKJFeU'});Pixlee.addSimpleWidget({widgetId:'23905'});};</script><script src="//instafeed.assets.pixlee.com/assets/pixlee_widget_1_0_0.js"></script>
-      </div> -->
+      </div>
 
-       <?php include_once("footer.html"); ?>
+    <!-- footer -->
+    <?php include_once("footer.html"); ?>
 
-
-      <!-- Scripts -->
-      <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
-<script>
-  // compile the template
-  var template = Handlebars.compile("Handlebars <b>{{doesWhat}}</b>");
-  // execute the compiled template and print the output to the console
-  console.log(template({ doesWhat: "rocks!" }));
-</script>
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-      <script type="text/javascript" src="assets/javascript/lamana.js"></script>
-    </body>
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
+    <script>
+      // compile the template
+      var template = Handlebars.compile("Handlebars <b>{{doesWhat}}</b>");
+      // execute the compiled template and print the output to the console
+      console.log(template({ doesWhat: "rocks!" }));
+    </script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="assets/javascript/lamana.js"></script>
+  </body>
 </html>
