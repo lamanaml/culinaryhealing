7f991d4a1842bec37ea8dacc088559bcad949383
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-01-21 12:06:54 -0500

    added dropdown menu

---
+++ 1day.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+       <head>
+         <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-26808506-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-26808506-1');
+</script>
+
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Culinary Healing : 21-day cleanse</title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="assets/css/lamana.css">
+    </head>
+    <body>
+      <!-- Navigation -->
+      <div class="container">
+        <nav class="navbar fixed-top navbar-light bg-light"   >
+          <span class="navbar-brand mb-0 h1"><img href="index.html" src="assets/images/chlogo-tran.png" width="300px"></span>
+          <ul class="nav justify-content-end">
+            <li class="nav-item">
+              <a class="nav-link " href="#">Recipes</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Personal Chef </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Detox</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+              </li>
+            </ul>
+          </nav>
+      </div>
+     
+   
+       <!-- Footer -->
+      <div class="footer">  
+          © CULINARY HEALING  2006 ALL RIGHTS RESERVED | THIS WEBSITE WAS DESIGNED AND DEVELOPED BY <a href="http://www.lamanadonadelle.com">LAMANA DONADELLE</a> (YEAH THATS ME TOO)
+      </div>
+
+
+      <!-- Scripts -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <script type="text/javascript" src="assets/javascript/lamana.js"></script>
+    </body>
+</html>
