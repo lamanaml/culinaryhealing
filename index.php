46e74e0b0e1c1a0e3e2207fa372780f46a3310d4
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-01-21 18:53:38 -0500

    updated footer

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
     <title>Culinary Healing</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
     <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/lamana.css">
   </head>
   <body id="home">
@@ -85,8 +85,8 @@
         </div>
       </div> 
 
-    <!-- Footer -->
-    <?php include_once("footer.html"); ?>
+    <!-- Footer-->
+    <?php include_once("footer.html"); ?> 
 
 
     <!-- Scripts -->
