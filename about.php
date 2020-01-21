c1a845e0442efc2a5c42ea7b219d61e3cce988a9
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-01-21 11:56:08 -0500

    cleaned up code

--- about.php
+++ about.php
@@ -3,73 +3,56 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-       <head>
-         <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-26808506-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-26808506-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-26808506-1');
+    </script>
 
-  gtag('config', 'UA-26808506-1');
-</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Culinary Healing : About</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/lamana.css">
+  </head>
+  <body>
+    <!-- Navigation -->
+    <?php include_once("nav.html"); ?>
 
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <title>Culinary Healing : 21-day cleanse</title>
-      <meta name="description" content="">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-      <link rel="stylesheet" href="assets/css/lamana.css">
-    </head>
-    <body>
-      <!-- Navigation -->
-        <?php include_once("nav.html"); ?>
+    <!-- main content -->
+    <div class="container">
+      <div class="jumbotron">
+        <h1 class="display-4">About</h1>
+        <p class="lead">about</p>
+        <hr class="my-4">
+        Meet Chef LaMana
 
-      <!-- main content -->
-         <div class="container">
-<div class="jumbotron">
-  <h1 class="display-4">About</h1>
-  <p class="lead">about</p>
-  <hr class="my-4">
-​
+        OK, so you want to know about me... Well, I remember back in the day, my uncle Rico used to make the best-fried bologna sandwiches known to man. You know, the kind of bologna that had the red strip around it. He would fry it up in the pan to the point that it bubbled up in the middle, and he had to cut it with a knife.... Put that on white bread with mustard and I was in heaven!!! Is anybody with me! Hehehe... those were the days. I think back now and I have to ask... what the hell is bologna anyway?!?!
 
-Meet Chef LaMana
+        I tell this story just to let you know where I come from. I grew up in the south, Durham, NC... where food was food, there was no separation of healthy food and unhealthy food, good and bad. If you could eat it and it stopped you from being hungry, it was all good. On any given day we had up to 15 people in our house to feed, and money was not abundant, so off to the local Piggly Wiggly grocery store (I am not making this name up), we went to get food to feed the family. I don't have to tell you what types of foods we ended up with. But not to worry, my grandma would "put her foot" in whatever came through the door, and each meal was prepared with love and tasted delicious!! Pig feet, pork chops, fried chicken, white rice, liver and onions, chitterlings w/ hot sauce, fried fish on Fridays! Boy were we happy. Little did we know, we were setting our bodies up to fail us. How could we know? No one ever told my grandma that pork was bad, and fried foods would eventually kill us.
 
-​
+        Fast forward many years later... I met my husband in 2001. He was raised Muslim, so he did not eat pork. One day while we were dating, he sat down and explained the deal about pork... that it was a combination of a cat, rat, and pig (or something like that), and that eating it caused trichinosis, which are worms that invade our systems and cause all kinds of disease. I thought "how could anything that could make bacon be bad?", and "if that was true, why would they sell pork to us?", but anyway, I listened and I never ate pork again. That was the beginning of my food consciousness. A couple of years later, we went through a Sacred Woman and Sacred King Man initiation where we were required to become vegan for 12 weeks. During that time, I learned a tremendous amount about foods and its effects on my body and I started looking at the world through very different eyes. I had gained a profound clarity about life and food's role in it.
 
-OK, so you want to know about me... Well, I remember back in the day, my uncle Rico used to make the best-fried bologna sandwiches known to man. You know, the kind of bologna that had the red strip around it. He would fry it up in the pan to the point that it bubbled up in the middle, and he had to cut it with a knife.... Put that on white bread with mustard and I was in heaven!!! Is anybody with me! Hehehe... those were the days. I think back now and I have to ask... what the hell is bologna anyway?!?!
+        As I looked around, I noticed how many of the people in my cipher were sick, lost and stuck... and they didn't even know it. How could this be? They made no connection between the foods that they ate and the dis-ease that they harbored. They had been deceived about their food and sickness in a massive and scandalous way! Seeing this, I became passionate about showing people the truth about foods and letting them know that we have choices, and we have to take control of our own health and wellness. We have become dependent on grocery stores which sell us foods which are unfit for human consumption, doctors who are more motivated by profit than cures, and medicines which work against our body's ability to heal itself. We must inform ourselves and make better decisions about our health not only for us but to heal the generations that come after us and those that came before us. To me, this is not only about living a healthier life, but it's also about getting that clarity to understand what our life's purpose is and experiencing our strong and divine connection with the Creator.
 
-
-
-I tell this story just to let you know where I come from. I grew up in the south, Durham, NC... where food was food, there was no separation of healthy food and unhealthy food, good and bad. If you could eat it and it stopped you from being hungry, it was all good. On any given day we had up to 15 people in our house to feed, and money was not abundant, so off to the local Piggly Wiggly grocery store (I am not making this name up), we went to get food to feed the family. I don't have to tell you what types of foods we ended up with. But not to worry, my grandma would "put her foot" in whatever came through the door, and each meal was prepared with love and tasted delicious!! Pig feet, pork chops, fried chicken, white rice, liver and onions, chitterlings w/ hot sauce, fried fish on Fridays! Boy were we happy. Little did we know, we were setting our bodies up to fail us. How could we know? No one ever told my grandma that pork was bad, and fried foods would eventually kill us.
-
-
-
-Fast forward many years later... I met my husband in 2001. He was raised Muslim, so he did not eat pork. One day while we were dating, he sat down and explained the deal about pork... that it was a combination of a cat, rat, and pig (or something like that), and that eating it caused trichinosis, which are worms that invade our systems and cause all kinds of disease. I thought "how could anything that could make bacon be bad?", and "if that was true, why would they sell pork to us?", but anyway, I listened and I never ate pork again. That was the beginning of my food consciousness. A couple of years later, we went through a Sacred Woman and Sacred King Man initiation where we were required to become vegan for 12 weeks. During that time, I learned a tremendous amount about foods and its effects on my body and I started looking at the world through very different eyes. I had gained a profound clarity about life and food's role in it.
-
-
-
-As I looked around, I noticed how many of the people in my cipher were sick, lost and stuck... and they didn't even know it. How could this be? They made no connection between the foods that they ate and the dis-ease that they harbored. They had been deceived about their food and sickness in a massive and scandalous way! Seeing this, I became passionate about showing people the truth about foods and letting them know that we have choices, and we have to take control of our own health and wellness. We have become dependent on grocery stores which sell us foods which are unfit for human consumption, doctors who are more motivated by profit than cures, and medicines which work against our body's ability to heal itself. We must inform ourselves and make better decisions about our health not only for us but to heal the generations that come after us and those that came before us. To me, this is not only about living a healthier life, but it's also about getting that clarity to understand what our life's purpose is and experiencing our strong and divine connection with the Creator.
-
-​
-
-Well... this is my journey, join me if you can...
-
-  </div>
-</div>
-
-
-
+        ​Well... this is my journey, join me if you can...
+      </div>
+    </div>
 
     <!-- Footer -->
-      <?php include_once("footer.html"); ?>
+    <?php include_once("footer.html"); ?>
 
     <!-- Scripts -->
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-      <script type="text/javascript" src="assets/javascript/lamana.js"></script>
-    </body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="assets/javascript/lamana.js"></script>
+  </body>
 </html>
