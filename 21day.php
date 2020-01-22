b4b54304a72dc8b312d649964b788b0822af6336
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-01-22 01:13:54 -0500

    updated 21day

--- 21day.php
+++ 21day.php
@@ -27,8 +27,60 @@
     <!-- Navigation -->
     <?php include_once("nav.html"); ?>
 
-    <!-- main content -->
-    21-day
+ <!-- main content -->
+
+    <div class="container whitebg">
+      <div class="jumbotron whitebg">
+        <h1 class="display-4">21-Day Cleanse</h1>
+    
+              <p>
+           It's cleanse time!
+
+​</p><p>
+
+A good cleanse helps remove toxins from our body and helps us maintain a healthy weight. Although the body is designed to rid itself of toxins naturally, it can become overburdened. This cleanse will give our bodies additional support to expel toxins and manage weight, which is important to maintaining our health and wellbeing.
+</p><p>
+​
+
+During these 21 days, we will eat and drink REAL, unprocessed, natural food and juices (don't worry, we will learn what that means). There are no gimmicks and there are no pills to buy.  We will allow food to be our medicine, repairing us at the cellular level.  There are simple guidelines to follow each day, recipes and videos for extra help, and peer support to help keep us motivated.  All we need are discipline and willpower.
+</p><p>
+​
+
+If you need a little help to get on track to better health, join us.
+          </p>
+        <p>
+        <div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+        </p>
+        <p>
+        Price: $39
+         </p> <p>
+        <a href="#" class="btn btn-primary btn-lg disabled" role="button" aria-disabled="true">Register Now <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us20.list-manage.com","uuid":"7bd51cd7f1b65f154b476fb09","lid":"a57ff37c91","uniqueMethods":true}) })</script></a>
+        
+         </p>
 
 
     <!-- Footer -->
