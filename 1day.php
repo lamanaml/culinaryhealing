5b031d45714a58cc19f13f0aef70c5e62f7b9a36
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-01-22 14:11:22 -0500

    updated 1day

--- 1day.php
+++ 1day.php
@@ -15,7 +15,7 @@
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Culinary Healing : 1-Day Detox
+    <title>Culinary Healing : 1-Day Detox</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
