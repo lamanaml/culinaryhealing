395e4331a5bdbcbc15eb2af24b003f4b0017557b
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-05-30 09:49:00 -0400

    updated culinary healing page

--- 21day.php
+++ 21day.php
@@ -14,10 +14,12 @@
       gtag('config', 'UA-26808506-1');
     </script>
 
+
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Culinary Healing : 21-Day Cleanse</title>
-    <meta name="description" content="">
+    <meta name="description" content="Culinary Healing 21-Day Cleanse">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -114,7 +116,27 @@
         
    <!-- ------------------------------  Register --------------------------------------------- -->      
 
+<div id="paypal-button-container"></div>
 
+    <!-- Add the checkout buttons, set up the order and approve the order -->
+    <script>
+      paypal.Buttons({
+        createOrder: function(data, actions) {
+          return actions.order.create({
+            purchase_units: [{
+              amount: {
+                value: '45.00'
+              }
+            }]
+          });
+        },
+        onApprove: function(data, actions) {
+          return actions.order.capture().then(function(details) {
+            alert('Transaction completed by ' + details.payer.name.given_name);
+          });
+        }
+      }).render('#paypal-button-container'); // Display payment options on your web page
+    </script>
          <div class="alert alert-primary" role="alert">
            <h2>REGISTER NOW!!</h2>
             <h3>Price: $45 </h3> 
@@ -132,6 +154,8 @@
 
 
     <!-- Scripts -->
+     <script src="https://www.paypal.com/sdk/js?client-id=ATKDEIVAtir4OUrlgSVuwXw1QobVhwW5aQSAPLq4AwrldL5xZQN9ETMqYMxe6CjXGwENtudjkdi7kFHD"> // Replace YOUR_SB_CLIENT_ID with your sandbox client ID
+    </script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
