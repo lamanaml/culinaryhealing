24635712563759a737b947fe579e79353cdc8bdf
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-05-30 19:40:08 -0400

    added whats up with section

--- firststeps.php
+++ firststeps.php
@@ -56,7 +56,7 @@
     <!-- Cards -->
     
       <div class="card-group ">
-      <span class="blackheader"> The Phases</span>
+      <div class="blackheader"> The Phases</div>
         <div class="card shadow p-3 mb-5 bg-white rounded">
           <img src="assets/images/whole.jpg" class="card-img-top" class="rounded-bottom" alt="...">
           <div class="card-body">
