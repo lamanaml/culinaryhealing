f8e03cdfa7545e54870d89b3c75a908a21f6561a
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-06-11 00:21:30 -0400

    updated copy

---
+++ 21day/21day.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+    <link rel="icon" 
+      type="image/jpg" 
+      href="assets/images/favicon.jpg">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-26808506-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-26808506-1');
+    </script>
+
+
+  <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Ensures optimal rendering on mobile devices. -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- Optimal Internet Explorer compatibility -->
+</head>
+
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Culinary Healing : 21-Day Cleanse</title>
+    <meta name="description" content="Culinary Healing 21-Day Cleanse">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/lamana.css">
+  </head>
+  <body id="21day">
+    <!-- Navigation -->
+    <?php include_once("nav.html"); ?>
+
+<!-- 21-day -->
+
+    <div class="container whitebg">     
+      <div class="jumbotron whitebg">
+        <h1 class="display-4">21-Day Cleanse</h1>
+        
+        
+        <!-- <div class="flex-container">
+          <div class="space" > -->
+            <!-- <img src="assets/images/cleanseWPimage.png" width="500px" ></div> -->
+            <img src="assets/images/cleansewebcover.png" width="100%" >
+          <div ><p>This cleanse will help remove toxins from our bodies and help us maintain a healthy weight. We are exposed to toxins on a daily basis. From the air that we breath, to products that we use, to the foods that we eat.  Although our bodies ae designed to rid itself of these toxins naturally, it can become overburdened. Detox programs, like this cleanse, gives our bodies the support to get rid of these toxins and manage weight, which is important to our overall health and wellbeing.​</p>
+          
+           <p>During these 21 days, we will eat and drink REAL, unprocessed, natural food and juices (don't worry, we will learn what that means). There are no gimmicks and there are no pills to buy.  We will allow food to be our medicine, repairing us at the cellular level.  There are simple guidelines to follow each day, recipes and videos for extra help, and peer support to help keep us motivated.  If you need a little help to get on track to better health, join us..</p>
+           </div>
+          
+          
+        </div>
+        
+<!--         
+        <div class="row">
+          <div class="col-8"><p>This cleanse will help remove toxins from our bodies and help us maintain a healthy weight. We are exposed to toxins on a daily basis. From the air that we breath, to products that we use, to the foods that we eat.  Although our bodies ae designed to rid itself of these toxins naturally, it can become overburdened. Detox programs, like this cleanse, gives our bodies the support to get rid of these toxins and manage weight, which is important to our overall health and wellbeing.​</p>
+              
+              <p>During these 21 days, we will eat and drink REAL, unprocessed, natural food and juices (don't worry, we will learn what that means). There are no gimmicks and there are no pills to buy.  We will allow food to be our medicine, repairing us at the cellular level.  There are simple guidelines to follow each day, recipes and videos for extra help, and peer support to help keep us motivated.  If you need a little help to get on track to better health, join us..</p></div>
+          <div class="col-4"><img src="assets/images/cleanseWPimage.png" width="100%"></div>
+        </div> -->
+              
+   <!-- ------------------------------  The phases --------------------------------------------- -->
+         <div width="100%"> <img src="assets/images/chprocessweb.png" width="100%"></div>
+         
+        
+          <!-- <div class="card-group">
+          <div class="card">
+            <img class="card-img-top" src="assets/images/whole.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Whole Food</h5>
+              <p class="card-text">(Phase 1) Whole foods are simply whole, unprocessed foods that exist as close to their natural state as possible.</p>
+            </div>
+          </div>
+          <div class="card">
+            <img class="card-img-top" src="assets/images/raw3.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Live Food / Raw</h5>
+              <p class="card-text">(Phase 2) Live foods are uncooked fruits and vegetables, so the enzymes are still in tact. These enzymes repair and rebuild our bodies</p>
+            </div>
+          </div>
+          <div class="card">
+            <img class="card-img-top" src="assets/images/juice.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Liquids</h5>
+              <p class="card-text">(Phase 3) Fresh fruit and veggie juice, coconut water, broths, herbal tea will flood our bodies with nutrients!</p>
+            </div>
+          </div>  
+          <div class="card">
+            <img class="card-img-top" src="assets/images/vegetable-soup.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Water</h5>
+              <p class="card-text">(Phase 4) Distilled water to completely starve the toxins and kick the detox into the highest gear. </p>
+            </div>
+          </div>  
+        </div>   -->
+
+        
+<!-- ------------------------------  The components --------------------------------------------- -->
+        <h3>The Experience</h3>
+        <div>This cleanse is for people with all levels of fasting experience from first timers to the experts. It is virtual, so you can live anywhere in the world and still participate. There are several components that work together to create the total cleanse experience.
+        </div>
+        <div class="card-group">
+          <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+              <div class="card-header">First Steps</div>
+              <div class="card-body">
+              <p class="card-text">This initial time will help us ramp up our knowledge about clean healthy eating.  We will learn about the foods that help establish and maintain optimal health.  We will learn how to identify safe recipes, menu plan and grocery shop like a pro!  </p>
+              </div>
+            </div>    
+
+            <div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
+                <div class="card-header">Daily Notes</div>
+                <div class="card-body">
+                  <p class="card-text">Once we are empowered with our goal, we will start the journey.  You will receive daily notes of information, encouragement and tips to keep us all on track! </p>
+                </div>
+            </div>
+
+            <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+                <div class="card-header">Group Discussion</div>
+                <div class="card-body">
+                  <p class="card-text">There is a PRIVATE discussion group where current members of the cleanse can support each other.  We ask and answer questions, share our experiences, and motivate each other to keep pushing forward. </p>
+                </div>
+            </div>
+
+            <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                <div class="card-header">Recipes</div>
+                <div class="card-body">
+                  <p class="card-text">There are several recipes for each phase of the cleanse.  You can test some out and find your favorites, or add your own! </p>
+                </div>
+            </div>
+
+            <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header">Hotline</div>
+                <div class="card-body">
+                  <p class="card-text">Our facilitators will be available 24-7 to support us all through the cleanse.  Through phone call, email, chat or through the group discussion, you are never alone!</p>
+                </div>
+            </div>
+          </div>
+        
+   <!-- ------------------------------  Register --------------------------------------------- -->      
+<div class="alert alert-success" role="alert" >
+
+<div class="row" >
+    <div class="col text-center">
+     <h3 class="text-center" >REGISTER NOW!!</h3>
+            <h3>Price: $45 </h3> 
+            <h2>July 10 - 30</h2> 
+
+      <div>
+         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+          <input type="hidden" name="cmd" value="_s-xclick">
+          <input type="hidden" name="hosted_button_id" value="WMNXR8LR85MQ8">
+          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+          </form>
+
+      </div>
+
+
+    </div>
+    <div class="col">
+      <h6>DISCLAIMER: Do not participate in this cleanse if you are pregnant, nursing or have chronic medical conditions.  Although this cleanse is intended to improve overall health, it is not a replacement for professional medical care.  We are not medical doctors. If you are on medication, consult your doctor before participating.  By registering for this cleanse, you agree to hold harmless Culinary Healing and any of its employees for any issues while participating in this cleanse. 
+          </h6>
+    </div>
+  </div>
+           
+          
+        </div>
+      </div>
+   </div>
+   
+   
+    <!-- Footer -->
+    <?php include_once("footer.html"); ?>
+
+
+    <!-- Scripts -->
+    
+     
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="assets/javascript/lamana.js"></script>
+  </body>
+</html>
