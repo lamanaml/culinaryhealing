6ca3ccb8bbe820f692b171b24c0a7449e93c6210
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-06-23 00:14:59 -0400

    added welcome page

--- 21days/firststeps.php
+++ 21days/firststeps.php
@@ -27,7 +27,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Pacifico&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="http://www.culinaryhealing.com/assets/css/lamana.css">
-    <link rel="stylesheet" href="assets/css/21days.css">
+    <link rel="stylesheet" href="assets/css/21day.css">
  
   </head>
 
