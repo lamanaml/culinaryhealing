772de8ec036deab9d9ab4549a13c9c24f2d981c7
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-07-02 13:32:30 -0400

    updated jumbotron

--- 21days/index.php
+++ 21days/index.php
@@ -41,7 +41,6 @@
    
 <!-- Hero -->
     <div class="container">
-      <div class="jumbotron2" >
         <img src="http://www.culinaryhealing.com/21days/assets/images/21daywelcomecover.png" width="100%" > 
         <blockquote>
           <p class="quotation">  "We are not human beings on a spiritual journal, we are spiritual beings on a human journey". 
