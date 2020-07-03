6ebf223d121662f675d901fea327163f0ea6b9c8
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-07-02 21:25:01 -0400

    updated whole food

--- 21days/index.php
+++ 21days/index.php
@@ -52,7 +52,7 @@
             
             
 <!-- Cards -->
-        <div class="container" style="margin-top:150px; background-color: white;">
+        <div class="container" style="margin-top:20px; background-color: white;">
       <div class="card-group ">
         <div class="card shadow col-sm-6 p-3 mb-5 bg-white rounded">
           <img src="../../assets/images/profile-lamana1.jpg" class="card-img-top" class="rounded-bottom" alt="...">
