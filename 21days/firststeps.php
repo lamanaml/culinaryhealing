69c54cb1ad0fa9fa5631196487c89f480989d4bf
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-07-02 22:07:09 -0400

    updated whole food

--- 21days/firststeps.php
+++ 21days/firststeps.php
@@ -92,7 +92,7 @@
    </div>
 
 <!-- Whats the deal with section -->
-      <div class="row "style="padding:50px;">
+      <div class="row ">
         <div class="col-sm-8">
           <div class="card">
             <div class="card-body">
