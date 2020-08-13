60a7c0f3601ddf0049bc295f06303cd98e9bc3b1
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-08-13 10:19:20 -0400

    updated css

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
   <head>
-    <link rel="icon"  type="image/jpg" href="assets/images/favicon.jpg">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Culinary Healing</title>
+    <link rel="icon"  type="image/jpg" href="assets/images/favicon.jpg">
     <meta name="Culinary Healing, Plant Based Lifestyle Brand" content="plant-based, vegan, vegetarian, 21-day, cleanse, detox, recipes, lamana, lamanaml, brooklyn, durham, atlanta, burlington">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-26808506-1"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
@@ -20,44 +20,52 @@
       gtag('config', 'UA-26808506-1');
     </script>
 
-    
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Pacifico&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/lamana.css">
+    <!-- styles -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">  
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="assets/css/lamana.css">
+      <link rel="stylesheet" href="assets/css/mq.css">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Pacifico&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">      
+    <!-- end of styles -->
   </head>
 
 
-  <body id="home">
-    <div class="nav-div">
+
+
+   
     
-<!-- navigation -->
-      <?php include_once("nav.html"); ?>
-    </div>
+
+
+
+  <body id="home">
+
+    <!-- Navigation -->
+      <div class="nav-div">
+        <?php include_once("nav.html"); ?>
+      </div>
+    <!-- end of navigation -->
      
-<!-- index -->
-
-
-<!-- Hero -->
+    <!-- Header -->
       <div class="hero-image">
         <div class="hero-text">
           <h1>Eat Better... Feel Better</h1>
-
           <p>Join our next 21-DAY CLEANSE - October 2 - 22, 2020</p>
-        <p> <a href="21day.php" class="btn btn-primary">Learn More</a></p>
+          <p><a href="21day.php" class="btn btn-primary">Learn More</a></p>
         </div>
       </div>
-
-
-      <!-- Subscribe -->
+    <!-- end of header -->
+   
+    <!-- Subscribe -->
       <?php include_once("subscribe.html"); ?>
+    <!-- end of Subscribe -->
+
+    <!-- Main content area -->
+      <div class="container">
 
       <!-- Cards -->
-      <div class="container">
         <div class="card-group ">
-
           <div class="card shadow p-3 mb-5 bg-white rounded">
             <img src="assets/images/cleanseoct.png" class="card-img-top" class="rounded-bottom" alt="...">
             <div class="card-body">
@@ -71,39 +79,34 @@
             <div class="card-body">
               <h5 class="card-title">About Culinary Healing</h5>
               <p class="card-text">Culinary Healing is a vegan / vegetarian lifestyle brand that demonstrates that we can have an extremely deliciously healthy life on a plant-based diet.     </p>
-               <p> <a href="about.php" class="btn btn-primary">Read More</a></p>
+              <p> <a href="about.php" class="btn btn-primary">Read More</a></p>
             </div>
           </div>
-          
-          <!-- <div class="card shadow p-3 mb-5 bg-white rounded">
-            <img src="assets/images/vegetable-soup.jpg" class="card-img-top"  class="rounded-bottom"alt="imag">
-            <div class="card-body">
-              <h5 class="card-title">Recipes</h5>
-              <p class="card-text">Take a tour of the food that I love to cook and eat.  I am not a huge recipe follower.  I sometimes use recipes as my guide, then put my own spin on them.  I encourage you to use these as a starting point, then make your own magic! <a href="recipes.php">Eat More...</a> </p>
-            </div>
-          </div> -->
         </div>
-  
+      <!-- end of Cards -->
 
-      <!-- instagram -->
-      <div class="container feed" >
-        <div class="powr-instagram-feed" id="1c268b0b_1579652448"></div><script src="https://www.powr.io/powr.js?platform=html"></script>        
+      <!-- instagram feed-->
+        <div class="container feed" >
+          <div class="powr-instagram-feed" id="1c268b0b_1579652448"></div>
+          <script src="https://www.powr.io/powr.js?platform=html"></script>        
+        </div>
+      <!-- end of instagram feed-->
+
       </div>
+    <!-- End of main content area -->
 
-
-
-  </div>
-<div>
     <!-- Footer-->
-    <?php include_once("footer.html"); ?> 
+      <div>
+        <?php include_once("footer.html"); ?> 
+      </div>
+    <!-- end of footer -->
 
-</div>
+    <!-- Scripts -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <script type="text/javascript" src="assets/javascript/lamana.js"></script>
+    <!-- end of scripts -->
 
-
-</script>
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="assets/javascript/lamana.js"></script>
   </body>
 </html>
