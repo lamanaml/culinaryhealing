901e140a2128965c603c281fb5531cbf8462478a
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-09-26 17:29:38 -0400

    updated cards

--- 21day.php
+++ 21day.php
@@ -86,12 +86,12 @@
   <div class="ft-recipe__thumb">
     <!-- <span id="close-modal"><i class="ion ion-md-close"></i> -->
     </span>
-    <h3>Phase 1</h3><img src="https://zippypaws.com/app/uploads/2018/05/strawberry-waffles-1024x668.jpg" alt="First steps"/>
+    <h3>Phase 2</h3><img src="https://zippypaws.com/app/uploads/2018/05/strawberry-waffles-1024x668.jpg" alt="Blast Off"/>
   </div>
   <div class="ft-recipe__content">
     <header class="content__header">
       <div class="row-wrapper">
-        <h2 class="recipe-title">First Steps</h2>
+        <h2 class="recipe-title">Blast Off</h2>
         <div class="user-rating"></div>
       </div>
       <!-- <ul class="recipe-details">
@@ -101,22 +101,23 @@
       </ul> -->
     </header>
     <p class="description">
-       Empower ourselves during this boot-camp style learning session to prepare for a successful cleanse.</p>
+      Feast on healthy, nutrient dense whole and raw food for a smooth transition into the detox.</p>
     <!-- <footer class="content__footer"><a href="#">View Recipe</a></footer> -->
   </div>
 </div></div>
 
 
-  <div><div class="ft-recipe cardBody cardHTML"> 
+  <div>
+    <div class="ft-recipe cardBody cardHTML"> 
   <div class="ft-recipe__thumb">
     <!-- <span id="close-modal"><i class="ion ion-md-close"></i> -->
     </span>
-    <h3>Phase 1</h3><img src="https://zippypaws.com/app/uploads/2018/05/strawberry-waffles-1024x668.jpg" alt="First steps"/>
+    <h3>Phase 3</h3><img src="https://zippypaws.com/app/uploads/2018/05/strawberry-waffles-1024x668.jpg" alt="Liquid Level Up"/>
   </div>
   <div class="ft-recipe__content">
     <header class="content__header">
       <div class="row-wrapper">
-        <h2 class="recipe-title">First Steps</h2>
+        <h2 class="recipe-title">Liquid Level Up</h2>
         <div class="user-rating"></div>
       </div>
       <!-- <ul class="recipe-details">
@@ -126,13 +127,63 @@
       </ul> -->
     </header>
     <p class="description">
-       Empower ourselves during this boot-camp style learning session to prepare for a successful cleanse.</p>
+       Freshly pressed liquids will thrust our bodies into full detox mode while nourishing us to the core.</p>
     <!-- <footer class="content__footer"><a href="#">View Recipe</a></footer> -->
   </div>
 </div></div>
 </div>
 
 
+  <div>
+ <div class="ft-recipe cardBody cardHTML"> 
+  <div class="ft-recipe__thumb">
+    <!-- <span id="close-modal"><i class="ion ion-md-close"></i> -->
+    </span>
+    <h3>Phase 4</h3><img src="https://zippypaws.com/app/uploads/2018/05/strawberry-waffles-1024x668.jpg" alt="Live for Five"/>
+  </div>
+  <div class="ft-recipe__content">
+    <header class="content__header">
+      <div class="row-wrapper">
+        <h2 class="recipe-title">Live for Five</h2>
+        <div class="user-rating"></div>
+      </div>
+      <!-- <ul class="recipe-details">
+        <li class="recipe-details-item time"><i class="ion ion-ios-clock-outline"></i><span class="value">20</span><span class="title">Minutes</span></li>
+        <li class="recipe-details-item ingredients"><i class="ion ion-ios-book-outline"></i><span class="value">5</span><span class="title">Ingredients</span></li>
+        <li class="recipe-details-item servings"><i class="ion ion-ios-person-outline"></i><span class="value">4-6</span><span class="title">Serving</span></li>
+      </ul> -->
+    </header>
+    <p class="description">
+     Enzyme Rich fruits and veggies will nourish us at the cellular level.</p>
+    <!-- <footer class="content__footer"><a href="#">View Recipe</a></footer> -->
+  </div>
+</div></div>
+
+
+  <div>
+ <div class="ft-recipe cardBody cardHTML"> 
+  <div class="ft-recipe__thumb">
+    <!-- <span id="close-modal"><i class="ion ion-md-close"></i> -->
+    </span>
+    <h3>Phase 5</h3><img src="https://zippypaws.com/app/uploads/2018/05/strawberry-waffles-1024x668.jpg" alt="Final Fine Tune"/>
+  </div>
+  <div class="ft-recipe__content">
+    <header class="content__header">
+      <div class="row-wrapper">
+        <h2 class="recipe-title">Final Fine Tune</h2>
+        <div class="user-rating"></div>
+      </div>
+      <!-- <ul class="recipe-details">
+        <li class="recipe-details-item time"><i class="ion ion-ios-clock-outline"></i><span class="value">20</span><span class="title">Minutes</span></li>
+        <li class="recipe-details-item ingredients"><i class="ion ion-ios-book-outline"></i><span class="value">5</span><span class="title">Ingredients</span></li>
+        <li class="recipe-details-item servings"><i class="ion ion-ios-person-outline"></i><span class="value">4-6</span><span class="title">Serving</span></li>
+      </ul> -->
+    </header>
+    <p class="description">
+     Bring our systems back up to speed with our new healthier heating habits.</p>
+    <!-- <footer class="content__footer"><a href="#">View Recipe</a></footer> -->
+  </div>
+</div></div>
 
 
 
