0f80fd019c9e16c5e0b903694812ed8f57724ba2
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-09-26 16:14:53 -0400

    updated cards

--- 21day.php
+++ 21day.php
@@ -54,24 +54,26 @@
          
 
  <div class="ft-recipe cardBody cardHTML"> 
-  <div class="ft-recipe__thumb"><span id="close-modal"><i class="ion ion-md-close"></i></span>
-    <h3>Today's Featured Recipe</h3><img src="https://zippypaws.com/app/uploads/2018/05/strawberry-waffles-1024x668.jpg" alt="Strawberry Waffle"/>
+  <div class="ft-recipe__thumb">
+    <!-- <span id="close-modal"><i class="ion ion-md-close"></i> -->
+    </span>
+    <h3>First Steps</h3><img src="https://zippypaws.com/app/uploads/2018/05/strawberry-waffles-1024x668.jpg" alt="First steps"/>
   </div>
   <div class="ft-recipe__content">
     <header class="content__header">
       <div class="row-wrapper">
-        <h2 class="recipe-title">Strawberry Waffle</h2>
+        <h2 class="recipe-title">First Steps</h2>
         <div class="user-rating"></div>
       </div>
-      <ul class="recipe-details">
+      <!-- <ul class="recipe-details">
         <li class="recipe-details-item time"><i class="ion ion-ios-clock-outline"></i><span class="value">20</span><span class="title">Minutes</span></li>
         <li class="recipe-details-item ingredients"><i class="ion ion-ios-book-outline"></i><span class="value">5</span><span class="title">Ingredients</span></li>
         <li class="recipe-details-item servings"><i class="ion ion-ios-person-outline"></i><span class="value">4-6</span><span class="title">Serving</span></li>
-      </ul>
+      </ul> -->
     </header>
     <p class="description">
-       There’s no better way to celebrate May being National Strawberry Month than by sharing a sweet treat with your pup!!! Strawberries...</p>
-    <footer class="content__footer"><a href="#">View Recipe</a></footer>
+       Empower ourselves during this boot-camp style learning session to prepare for a successful cleanse.</p>
+    <!-- <footer class="content__footer"><a href="#">View Recipe</a></footer> -->
   </div>
 </div>
    </div>
