27976d4c5ded8389b3033cebb8072dbd309b9aa5
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-10-26 14:34:18 -0400

    added recipe cards

---
+++ recipes/index.php
@@ -0,0 +1,65 @@
+  <head>
+    <link rel="icon"  type="image/jpg" href="assets/images/favicon.jpg">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Recipes - Culinary Healing</title>
+    <meta name="Culinary Healing, Plant Based Lifestyle Brand" content="vegan meals near me, healthy plant-based, vegan, vegetarian, 21-day, cleanse, detox, recipes, lamana, lamanaml, brooklyn, durham, atlanta, burlington">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-26808506-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-26808506-1');
+    </script>
+
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/dist/lamana.css">
+    
+  </head>
+
+
+  <body id="recipes">
+    <!-- navigation -->
+    <div class="nav-div">
+      <?php include_once("../includes/nav.html"); ?>
+    </div>
+     
+<!-- index -->
+
+   <!-- main content -->
+
+    <div class="card-container">
+  <div class="card u-clearfix">
+    <div class="card-body">
+      <span class="card-number card-circle subtle">01</span>
+      <span class="card-author subtle">John Smith</span>
+      <h2 class="card-title">New Brunch Recipe</h2>
+      <span class="card-description subtle">These last few weeks I have been working hard on a new brunch recipe for you all.</span>
+      <div class="card-read">Read</div>
+      <span class="card-tag card-circle subtle">C</span>
+    </div>
+    <img src="https://s15.postimg.cc/temvv7u4r/recipe.jpg" alt="" class="card-media" />
+  </div>
+  <div class="card-shadow"></div>
+</div>
+   
+
+
+      <!-- Footer -->
+      <?php include_once("../includes/footer.html"); ?>
+
+      <!-- Scripts -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <script type="text/javascript" src="../assets/javascript/lamana.js"></script>
+   </body>
+</html>
