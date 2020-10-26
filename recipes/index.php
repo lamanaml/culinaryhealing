2991467d85e5f448d0bc73f4d4d374090ccf5e41
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-10-26 16:07:07 -0400

    updated php path

--- recipes/index.php
+++ recipes/index.php
@@ -42,21 +42,7 @@
 
    <!-- main content -->
 
-    <div class="card-container">
-  <div class="card u-clearfix">
-    <div class="card-body">
-      <span class="card-number card-circle subtle">01</span>
-      <span class="card-author subtle">John Smith</span>
-      <h2 class="card-title">New Brunch Recipe</h2>
-      <span class="card-description subtle">These last few weeks I have been working hard on a new brunch recipe for you all.</span>
-      <div class="card-read">Read</div>
-      <span class="card-tag card-circle subtle">C</span>
-    </div>
-    <img src="https://s15.postimg.cc/temvv7u4r/recipe.jpg" alt="" class="card-media" />
-  </div>
-  <div class="card-shadow"></div>
-</div>
-   
+
 
 <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
