7f52afcde862ed40816e33eae534903b43bcb466
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-10-26 16:28:54 -0400

    updated cards

--- recipes.php
+++ recipes.php
@@ -29,7 +29,7 @@
   <body id="recipes">
     <!-- navigation -->
     <div >
-      <!-- <?php include_once("../includes/nav.html"); ?> -->
+      <!-- <?php include_once("includes/nav.html"); ?> -->
     </div>
      
 <!-- index -->
@@ -63,7 +63,7 @@
   </div>
 </div>
       <!-- Footer -->
-      <?php include_once("../includes/footer.php"); ?>
+      <?php include_once("includes/footer.php"); ?>
 
       <!-- Scripts -->
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
