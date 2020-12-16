cce0d0bf50f44a5188d95cb417d60cb19b775460
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-12-15 22:00:21 -0500

    updated cards

---
+++ index1.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+    <link rel="icon"  type="image/jpg" href="assets/images/favicon.jpg">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>21-Day Cleanse - Welcome - Culinary Healing</title>
+    <meta name="robots" content="noimageindex, noindex, nofollow, nosnippet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-26808506-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-26808506-2');
+  </script>
+
+
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
+
+    
+    <link rel="stylesheet" href="assets/css/21day.css">
+  </head>
+
+
+  <body id="welcome">
+    
+
+<!-- navigation -->
+    <div class="nav-div">
+      <?php include_once("includes/nav.html"); ?>
+    </div>
+     
+   
+<!-- Header -->
+    <img class="headerImg" src="assets/images/pagetitles/welcome.png" width="100%" > 
+
+      
+   <!-- content Body -->
+        <div class="container intro">  
+        <p class="card-text">  One little known fact is that our bodies naturally detox each and every day.  Unfortunately, the number of toxins that we consume far outweigh what our bodies can naturally detox.  During these 3 weeks, we will clean up our diets to reduce the number of toxins that we consume, so that our bodies can detox more efficiently. During this time we will learn a lot about food, our relationship to it, and the effects that different foods have on our bodies.</p>
+
+        <p class="card-text"> This cleanse is organized to be gradual so that we experience a minimal amount of detox symptoms. The first week is designed to slowly release toxins that have been stored in our bodies for many years. During the second week we will flood our systems with liquids that provide nutrients that will heal and rebuild our bodies on a cellular level. During the third week we will slowly reintroduce foods and make notes of how they make us feel. Using this feedback from our bodies, we will be able to optimize our diets. </p>
+
+        <p class="card-text">Each one of us will have a slightly different experience with some days being more challenging than others, but  through this we will see what we are made of (literally).  We will purge, heal and rejoice.  We will cry and we will laugh.  We will break through the stronghold that food and toxins have had on us, and start a new journey... stronger, healthier and free. Let's go! </p>
+       
+      </div>
+
+
+      <div class="nav-div">
+        <?php include_once("includes/steps.html"); ?>
+      </div>
+
+<!-- Footer-->
+       <div>
+      <?php include_once("includes/footer.html"); ?> 
+     </div>
+
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="assets/js/21day.js"></script>
+  </body>
+</html>
