946e3fbd8f454a7be9063fc0ffe3a1af3f026636
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2021-01-02 21:28:47 -0500

    updated index

--- index.php
+++ index.php
@@ -1,129 +1,135 @@
-<?php   
- session_start();  
- ?>  
-
- <!DOCTYPE html>  
- <html>  
-      <head>  
-           <title>Webslesson Tutorial | Make Login Form by Using Bootstrap Modal with PHP Ajax Jquery</title>  
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> 
-<script type = "text/javascript"
-src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-
-<script type = "text/javascript" language = "javascript">
-
-$(document).ready(function() {
-var surl = "http://localhost:8080/alfresco/service/api/login.json?u=admin&pw=admin";
-$.ajax({
-url: surl,
-dataType: "json",
-success: function (data) {
-alert(JSON.stringify(data));
-}
-});
-});
-
-</script>
-      </head>  
-      <body>  
-           <br />  
-           <div class="container" style="width:700px;">  
-                <h3 style="align:center">Make Login Form by Using Bootstrap Modal with PHP Ajax Jquery</h3><br />  
-                <br />  
-                <br />  
-                <br />  
-                <br />  
-                <br />  
-                <?php  
-                if(isset($_SESSION['username']))  
-                {  
-                ?>  
-                <div style="align:center">  
-                     <h1>Welcome - <?php echo $_SESSION['username']; ?></h1><br />  
-                     <a href="#" id="logout">Logout</a>  
-                </div>  
-                <?php  
-                }  
-                else  
-                {  
-                ?>  
-                <div style="align:center">  
-                     <button type="button" name="login" id="login" class="btn btn-success" data-toggle="modal" data-target="#loginModal">Login</button>  
-                </div>  
-                <?php  
-                }  
-                ?>  
-           </div>  
-           <br />  
-      </body>  
- </html>  
- <div id="loginModal" class="modal fade" role="dialog">  
-      <div class="modal-dialog">  
-   <!-- Modal content-->  
-           <div class="modal-content">  
-                <div class="modal-header">  
-                     <button type="button" class="close" data-dismiss="modal">&times;</button>  
-                     <h4 class="modal-title">Login</h4>  
-                </div>  
-                <div class="modal-body">  
-                     <label>Username</label>  
-                     <input type="text" name="username" id="username" class="form-control" />  
-                     <br />  
-                     <label>Password</label>  
-                     <input type="password" name="password" id="password" class="form-control" />  
-                     <br />  
-                     <button type="button" name="login_button" id="login_button" class="btn btn-warning">Login</button>  
-                </div>  
-           </div>  
-      </div>  
- </div> 
 
 
- <script>  
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+
+  <head>
+    <meta charset="utf-8">
+
+    <title>Culinary Healing</title>
+    <meta http-equiv="X-UA-Compatible" rel=canonical” content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="follow, index">
+    <link rel="canonical" href="https://www.culinaryhealing.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="Chef LaMana follows a plant based whole food philosophy to create delicious meals.  She uses classes, detox program and recipes to show the world that a vegan, vegetarian lifestyle does not have to be bland."  />
+    <meta name="author" content="LaMana Donadele" /> 
+    <meta property="og:site_name" content="Culinary Healing"/>
+    <meta property="og:title" content="Culinary Healing"/>
+    <meta property="og:url" content="https://culinaryhealing.com/"/>
+    <meta property="og:type" content="website"/>
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://culinaryhealing.com/">
+    <meta property="og:title" content="Culinary Healing">
+    <meta property="og:description" content="Chef LaMana follows a plant based whole food philosophy to create delicious meals.  She uses classes, detox program and recipes to show the world that a vegan, vegetarian lifestyle does not have to be bland.">
+    <meta property="og:image" content="">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://culinaryhealing.com/">
+    <meta property="twitter:title" content="Culinary Healing">
+    <meta property="twitter:description" content="Chef LaMana follows a plant based whole food philosophy to create delicious meals.  She uses classes, detox program and recipes to show the world that a vegan, vegetarian lifestyle does not have to be bland.">
+    <meta property="twitter:image" content="">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-26808506-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-26808506-1');
+    </script>
+
+    <!-- styles -->
+    <link rel="icon"  type="image/jpg" href="assets/images/favicon.jpg">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> 
+     <link rel="stylesheet" href="assets/css/dist/lamana.css">
+    <!-- end of styles -->
+  </head>
+
+  <body id="home">
+
+<!-- //////////////////////////// Navigation  ////////////////////////////////////////////-->
+      
+  
+<?php include('includes/nav.php'); ?>
+
  
- $(document).ready(function(){  
-      $('#login_button').click(function(){  
-           var username = $('#username').val();  
-           var password = $('#password').val();  
-           if(username != '' && password != '')  
-           {  
-                $.ajax({  
-                     url:"action.php",  
-                     method:"POST",  
-                     data: {username:username, password:password},  
-                     success:function(data)  
-                     {  
-                          //alert(data);  
-                          if(data == 'No')  
-                          {  
-                               alert("Wrong Data");  
-                          }  
-                          else  
-                          {  
-                               $('#loginModal').hide();  
-                               location.reload();  
-                          }  
-                     }  
-                });  
-           }  
-           else  
-           {  
-                alert("Both Fields are required");  
-           }  
-      });  
-      $('#logout').click(function(){  
-           var action = "logout";  
-           $.ajax({  
-                url:"action.php",  
-                method:"POST",  
-                data:{action:action},  
-                success:function()  
-                {  
-                     location.reload();  
-                }  
-           });  
-      });  
- });  
- </script>  
+
+<!-- ////////////////////// Masthead ////////////////////////////////  -->
+
+<div class="hero-home-image">
+  <div class="hero-text text-left">
+    <h1 class="h1 ">What's for Dinner?</h1>
+    <p class="h3">Try Delicious Plant-Based Meals </p>
+  </div>
+</div>
+
+<!-- ///////////////////////  Content  //////////////////////////////////  -->
+    <section >
+
+    <h2>What are we working on?</h2> 
+      <div class="container">
+        <div class="card-group ">
+          <div class="card  p-3 mb-5 bg-white rounded">
+            <img src="assets/images/3.jpg" class="card-img-top" class="rounded-bottom" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Meal Ideas</h5>
+              <p class="card-text">Let's face it, sometimes we can lose our inspiration for cooking... especially with a plant-based diet.  Hopefully, we can ignite your passion to jump back in with kitchen some simple and delicious meal ideas.  </p>  <p> <a href="meals.php" class="btn btn-warning">Learn more</a> </p>
+            </div>
+          </div>
+
+          <div class="card  p-3 mb-5 bg-white rounded">
+            <figure>
+            <img src="assets/images/11.jpg" class="card-img-top " class="rounded-bottom" alt="...">
+            </figure>
+            <div class="card-body">
+              <h5 class="card-title">21-Day Cleanse</h5>
+              <p class="card-text">Take a 21 day vacation from the toxins of the world. Let your body fight the battle against toxins, leaving you lighter, clearer and healthier! </p>  <p> <a href="21day.php" class="btn btn-warning">Learn more</a> </p>
+            </div>
+          </div>
+        </div> 
+      <div>
+    </section>
+
+
+
+  <section class="gray-background">
+    <h2>About Us</h2> 
+    <div class="container parent" >
+      <div class="p-3 mb-5  rounded" >
+        <img src="assets/images/features/whole3.jpg" class="card-img-top" class="rounded-bottom" alt="...">
+      </div>
+      <div class="overlay">
+        <h5 >About Culinary Healing</h5>
+        <p class="card-text">Culinary Healing is a vegan / vegetarian lifestyle brand that demonstrates that we can have an extremely deliciously healthy life on a plant-based diet.     </p>
+        <p> <a href="about.php" class="btn btn-warning">Read More</a></p>
+      </div>
+    </div>
+  </section>
+
+
+<!-- /////////////////////////////  Footer   ///////////////////////  -->
+   
+ 
+<?php include('includes/footer.php'); ?>
+
+ 
+
+
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="assets/javascript/lamana.js"></script>
+  </body>
+</html>
